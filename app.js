9605f8425d97c3c9321383477987a103b8edee2d
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-04 16:09:50 +0200

    css debug dayclothes debug

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const session = require("express-session");
 const passport = require("passport");
 
 mongoose
-  .connect("mongodb://localhost/dressingproject", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
